adb2796db452305ac5a9d2d8269ee3a30e38168f
Abhilash4269 <abhilash.haranadha@gmail.com>
2022-01-19 12:04:42 +0530

    first commit of my TestDemo repo

---
+++ 5_class.js
@@ -0,0 +1,18 @@
+function foo(arr, b) {
+    if (b === undefined)
+        return arr[0]
+
+    if (n < 0)
+        return [];
+
+    return arr.slice(0, b);
+}
+// else if b >=1 { var temp =[];   ### alternate method
+//     for(var i=0;i<=arr.length;i++){
+//       temp.push(arr[i])
+//     }
+//     return temp
+
+//arrow function
+// ES6 - latest version of js
+// syntax :variable=()=>{}
